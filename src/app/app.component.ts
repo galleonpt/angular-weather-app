5012daf1bde7c65d5e909f444439e6b58a37cf39
galleonpt <jose15.luis00@gmail.com>
2024-05-25 17:51:47 +0100

    feat: form for search user name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 import { GithubApiService } from './services/github-api.service';
+
+interface ISearchForm {
+    name: string;
+}
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-    constructor(private githubApiService: GithubApiService) {}
+    searchForm: FormGroup;
+    searchedValue: string = '';
+
+    constructor(
+        private githubApiService: GithubApiService,
+        private fb: FormBuilder
+    ) {
+        this.searchForm = this.fb.group<ISearchForm>({
+            name: '',
+        });
+    }
 
     ngOnInit(): void {
         this.githubApiService
             .getUserInfo()
             .subscribe((data) => console.log(data));
+    }
+
+    onSubmit() {
+        this.searchedValue = this.searchForm.value.name;
     }
 }
